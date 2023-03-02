af3bf154b12321a21402799544d6da9aadb9b881
Juned Memon <junaid18183@gmail.com>
2023-03-02 17:54:50 +0530

    Added MicroService

--- main.go
+++ main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Application")
 		os.Exit(1)
 	}
+	if err = (&controllers.MicroServiceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MicroService")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
