4a74b742d59ee2e5ff46ddb817eb07d492e55a4f
Juned Memon <junaid18183@gmail.com>
2023-03-10 16:50:17 +0530

    Added ready and status in output

--- api/v1alpha1/catalog_types.go
+++ api/v1alpha1/catalog_types.go
@@ -20,6 +20,32 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// CrossNamespaceObjectReference contains enough information to let you locate
+// the typed referenced object at cluster level.
+type CrossNamespaceObjectReference struct {
+	// APIVersion of the referent.
+	// +optional
+	APIVersion string `json:"apiVersion,omitempty"`
+
+	// Kind of the referent.
+	// +kubebuilder:validation:Enum=HelmRepository;GitRepository;Bucket
+	// +required
+	Kind string `json:"kind,omitempty"`
+
+	// Name of the referent.
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=253
+	// +required
+	Name string `json:"name"`
+
+	// Namespace of the referent.
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Optional
+	// +optional
+	Namespace string `json:"namespace,omitempty"`
+}
+
 type ComponentSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
@@ -27,18 +53,14 @@ type ComponentSpec struct {
 	// +kubebuilder:validation:Required
 	// Role which can access the Catalog.
 	Name string `json:"name"`
-	// Github Repository of the Catalog.
-	Repository string `json:"repository,omitempty"`
-	// Gitlab ProjectID  of the Catalog.
-	ProjectId int16 `json:"project_id,omitempty"`
+	// The name and namespace of the gitrepositories.source.toolkit.fluxcd.io the Component is available at.
+	SourceRef CrossNamespaceObjectReference `json:"sourceRef,omitempty"`
 	// image_path of the Catalog.
 	ImagePath string `json:"image_path,omitempty"`
 	// variable_file_path of the Catalog.
 	VariableFilePath string `json:"variable_file_path,omitempty"`
 	// tool_type of the Component.
 	ToolType string `json:"tool_type,omitempty"`
-	// ref of the Catalog.
-	Ref string `json:"ref,omitempty"`
 }
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -56,8 +78,9 @@ type CatalogSpec struct {
 	Role string `json:"role"`
 	// Type of the Catalog.
 	Type string `json:"type"`
-	// Github Repository of the Catalog.
-	Repository string `json:"repository,omitempty"`
+	// The name and namespace of the gitrepositories.source.toolkit.fluxcd.io the Component is available at.
+	// +required
+	SourceRef CrossNamespaceObjectReference `json:"sourceRef"`
 	// values_folder_path of the Catalog.
 	ValuesFolderPath string `json:"values_folder_path,omitempty"`
 	// values_folder_path of the Catalog.
@@ -67,8 +90,6 @@ type CatalogSpec struct {
 	// readme_file_path of the Catalog.
 	ReadmeFilePath string `json:"readme_file_path,omitempty"`
 	// ref of the Catalog.
-	Ref string `json:"ref,omitempty"`
-	// ref of the Catalog.
 	Sops bool `json:"sops,omitempty"`
 	// components of the Catalog
 	Components []ComponentSpec `json:"components",omitempty`
@@ -76,8 +97,14 @@ type CatalogSpec struct {
 
 // CatalogStatus defines the observed state of Catalog
 type CatalogStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Conditions holds the conditions for the HelmRelease.
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// Failures is the reconciliation failure count against the latest desired
+	// state. It is reset after a successful reconciliation.
+	// +optional
+	Failures int64 `json:"failures,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -86,10 +113,12 @@ type CatalogStatus struct {
 // Catalog is the Schema for the catalogs API
 // +kubebuilder:validation:XPreserveUnknownFields
 // +kubebuilder:printcolumn:name=type,type=string,JSONPath=.spec.type
-// +kubebuilder:printcolumn:name=repository,type=string,JSONPath=.spec.repository
-// +kubebuilder:printcolumn:name=ref,type=string,JSONPath=.spec.ref
+// +kubebuilder:printcolumn:name=source_type,type=string,JSONPath=.spec.sourceRef.kind
+// +kubebuilder:printcolumn:name=source_name,type=string,JSONPath=.spec.sourceRef.name
 // +kubebuilder:printcolumn:name=role,type=string,JSONPath=.spec.role
 // +kubebuilder:printcolumn:name=age,type=date,JSONPath=.metadata.creationTimestamp
+// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description=""
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].message",description=""
 type Catalog struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
